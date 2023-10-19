6b6c32640ce333d9660d3d4e5f77b76b39434b57
KalidNadere <131591052+KalidNadere@users.noreply.github.com>
2023-10-19 20:30:39 +1100

    updated main.jsx to import react-router-dom & CreateComplaint, function for rendering React components onto the HTML page

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+// Import specific components from 'react-router-dom' library
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+// Import React component from 'CreateComplaint'
+import CreateComplaint from './components/CreateComplaint';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+  <Router>
+    <switch>
+      <Route exact path="/" component={App} />
+      <Route path="/create-complaint" component={CreateComplaint} />
+    </switch>
+  </Router>,
+  document.getElementById('root')
+  
+);
