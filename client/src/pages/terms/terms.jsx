f2b0e0248bf6da8e366df176aa360dd6fa13b757
MakeRedundant <105352286+MakeRedundant@users.noreply.github.com>
2023-10-19 20:23:53 +1100

    added contact, newpostpage, T and Cs

---
+++ client/src/pages/terms/terms.jsx
@@ -0,0 +1,14 @@
+import "./terms.css";
+
+const terms = () => {
+  return (
+    <section id='terms'>
+      <h1>Terms and conditions</h1>
+      <div className="text-box">
+        <p>Insert lack of Terms and conditions</p>
+      </div>
+    </section>
+  );
+};
+
+export default terms;
