1e991129c06763353a049567e03703e3ebc3e944
MakeRedundant <105352286+MakeRedundant@users.noreply.github.com>
2023-10-19 20:03:16 +1100

    Homepage

---
+++ client/src/pages/homepage/Homepage.jsx
@@ -0,0 +1,23 @@
+import './Homepage.css';
+
+const Homepage = () => {
+      return (
+    <header id="header" className="header">
+      <div className="header-container">
+        <div className="Header-section">
+          <h1>Let's Complain</h1>
+          <div className="header-text">
+            <p>
+
+            </p>
+            <p>Stuff here
+            </p>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+
+export default Homepage;
