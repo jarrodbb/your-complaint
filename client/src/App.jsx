cb63d9c65f95e4449a1a966ec53e461fe8901f9e
Jarrod Blanning <jarrod.blanning@gmail.com>
2023-10-21 19:36:19 +1000

    set up auth in utils and apollo in app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,15 +8,45 @@ import Header from "./components/header/header";
 import Footer from "./components/footer/footer";
 
 import { Outlet } from "react-router-dom";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+
+import { StoreProvider } from "./utils/GlobalState";
+
+const httpLink = createHttpLink({
+  uri: "/graphql",
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("id_token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 const App = () => {
   return (
-    <>
-      <Navbar />
-      <Header />
-      <Outlet />
-      <Footer />
-    </>
+    <ApolloProvider client={client}>
+      <StoreProvider>
+        <Navbar />
+        <Header />
+        <Outlet />
+        <Footer />
+      </StoreProvider>
+    </ApolloProvider>
   );
 };
 
