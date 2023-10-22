3c537cf3ff7ae857f7c05f5c12b177ed3b041f01
Jarrod Blanning <jarrod.blanning@gmail.com>
2023-10-22 16:44:53 +1000

    reducers fixed

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -22,62 +22,61 @@ export const reducer = (state, action) => {
         comments: [...action.comments],
       };
 
-    case UPDATE_COMPLAINT:
-      {
-        const complaintIndex = state.complaints.findIndex(
-          (complaint) => complaint.id === action.payload.id
-        );
+    case UPDATE_COMPLAINT: {
+      const complaintIndex = state.complaints.findIndex(
+        (complaint) => complaint.id === action.payload.id
+      );
 
-        const updateComplaint = {
-          ...state.complaints[complaintIndex],
-          ...action.payload,
-        };
+      const updateComplaint = {
+        ...state.complaints[complaintIndex],
+        ...action.payload,
+      };
 
-        const newComplaintsList = [...state.complaints]
+      const newComplaintsList = [...state.complaints];
 
-        new newComplaintsList[complaintIndex] = updateComplaint
-        return { 
-            ...state,
-            complaints: newComplaintsList
-        };
-      }
+      newComplaintsList[complaintIndex] = updateComplaint;
+      return {
+        ...state,
+        complaints: newComplaintsList,
+      };
+    }
 
-      case UPDATE_COMMENT: 
-      {
-        const commentIndex = state.commentss.findIndex(
-          (comment) => comment.id === action.payload.id
-        );
+    case UPDATE_COMMENT: {
+      const commentIndex = state.commentss.findIndex(
+        (comment) => comment.id === action.payload.id
+      );
 
-        const updateComment = {
-          ...state.comments[commentIndex],
-          ...action.payload,
-        };
+      const updateComment = {
+        ...state.comments[commentIndex],
+        ...action.payload,
+      };
 
-        const newCommentList = [...state.comments]
+      const newCommentList = [...state.comments];
 
-        new newCommentList[commentIndex] = updateComment
-        return { 
-            ...state,
-            comments: newCommentList
-        };
-      }
+      newCommentList[commentIndex] = updateComment;
+      return {
+        ...state,
+        comments: newCommentList,
+      };
+    }
 
-      case REMOVE_COMMENT: {
-        return {
-            ...state,
-            comments: [...state.comments].filter((comment) => comment.id !== action.payload)
-        }
-      }
+    case REMOVE_COMMENT: {
+      return {
+        ...state,
+        comments: [...state.comments].filter(
+          (comment) => comment.id !== action.payload
+        ),
+      };
+    }
 
-
-      case REMOVE_COMPLAINT: {
-        return {
-            ...state,
-            complaints: [...state.complaints].filter((complaint) => complaint.id !== action.payload)
-        }
-      }
-     
-     
+    case REMOVE_COMPLAINT: {
+      return {
+        ...state,
+        complaints: [...state.complaints].filter(
+          (complaint) => complaint.id !== action.payload
+        ),
+      };
+    }
 
     case UPDATE_ACCOUNT_STATUS:
       console.log("UPDATE_ACCOUNT_STATUS dispatched");
