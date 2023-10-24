7e1f4bb49673cc5fbda9e6ea5d297f519a85415d
MakeRedundant <105352286+MakeRedundant@users.noreply.github.com>
2023-10-24 20:14:23 +1100

    fixed post rendering

--- client/src/components/posts/Post.jsx
+++ client/src/components/posts/Post.jsx
@@ -8,18 +8,17 @@ import Card from "@mui/material/Card";
 import CardActionArea from "@mui/material/CardActionArea";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
-import {
-  createTheme,
-  ThemeProvider,
-  alpha,
-  getContrastRatio,
-} from "@mui/material/styles";
+// import {
+//   createTheme,
+//   ThemeProvider,
+//   alpha,
+//   getContrastRatio,
+// } from "@mui/material/styles";
 
-
+// import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 function Post({ complaint }) {
 
-  
   const commentLength = complaint.comments.length;
   console.log(commentLength);
 
@@ -31,36 +30,36 @@ function Post({ complaint }) {
   useEffect(() => {
     if (complaintCategory) {
       switch (complaintCategory) {
-        case "General":
-          setStyle("#ffb8b0");
-          break;
-        case "Food":
-          setStyle("#fdffba");
-          break;
-        case "Traffic":
-          setStyle("#b7ffff");
-          break;
-        case "Work":
-          setStyle("#ffd6c5");
-          break;
-        case "Finance":
-          setStyle("#ffffca");
-          break;
-        case "Life":
-          setStyle("#ffcdf8");
-          break;
-        case "Health":
-          setStyle("#ceebff");
-          break;
-        case "Technology":
-          setStyle("#c4ffd4");
-          break;
-        case "Random":
-          setStyle("#c9c4ff");
-          break;
-        default:
-          alert("I don't know such values");
-          return;
+      case "General":
+        setStyle("#ffb8b0");
+        break;
+      case "Food":
+        setStyle("#fdffba");
+        break;
+      case "Traffic":
+        setStyle("#b7ffff");
+        break;
+      case "Work":
+        setStyle("#ffd6c5");
+        break;
+      case "Finance":
+        setStyle("#ffffca");
+        break;
+      case "Life":
+        setStyle("#ffcdf8");
+        break;
+      case "Health":
+        setStyle("#ceebff");
+        break;
+      case "Technology":
+        setStyle("#c4ffd4");
+        break;
+      case "Random":
+        setStyle("#c9c4ff");
+        break;
+      default:
+        alert("I don't know such values");
+        return;
       }
     }
   }, []);
@@ -79,40 +78,37 @@ function Post({ complaint }) {
   //     },
   //   },
   // });
-
   return (
-    // <ThemeProvider theme={theme}>
-      <Grid item xs={12} md={6}>
-        <CardActionArea component="a" href="#">
-          <Card sx={{ bgcolor: "test.main", display: "flex" }}>
-            <CardContent sx={{ flex: 1 }}>
-              <Typography component="h2" variant="h5">
-                {complaint.category}
-                {console.log("test" + complaint.description)}
-              </Typography>
-              <Typography variant="subtitle1" color="text.secondary">
-                {complaint.date}
-              </Typography>
-              <Typography variant="subtitle1" paragraph>
-                {complaint.description}
-              </Typography>
-              <Typography variant="subtitle1" color="primary">
-                {commentLength} comments
-              </Typography>
-              <Typography variant="subtitle1" paragraph>
-                {complaint.createdAt}
-              </Typography>
-            </CardContent>
-            <CardMedia
-              component="img"
-              sx={{ width: 160, display: { xs: "none", sm: "block" } }}
-              image="https://i5.walmartimages.com/asr/9b971d54-7995-4a47-aa7a-adb2d7630c6c.f21033ccb62a1d89e93c2402428e6085.jpeg"
-              alt="text"
-            />
-          </Card>
-        </CardActionArea>
-      </Grid>
-    // </ThemeProvider>
+    <Grid item xs={12} md={12}>
+      <CardActionArea component="a" href="#">
+        <Card sx={{ display: "flex" }}>
+          <CardContent sx={{ flex: 1 }}>
+            <Typography component="h2" variant="h5">
+              {complaint.category}
+              {console.log("test" + complaint.description)}
+            </Typography>
+            <Typography variant="subtitle1" color="text.secondary">
+              {complaint.date}
+            </Typography>
+            <Typography variant="subtitle1" paragraph>
+              {complaint.description}
+            </Typography>
+            <Typography variant="subtitle1" color="primary">
+              {commentLength} Comments
+            </Typography>
+            <Typography variant="subtitle1" paragraph>
+              {complaint.createdAt}
+            </Typography>
+          </CardContent>
+          <CardMedia
+            component="img"
+            sx={{ width: 160, display: { xs: "none", sm: "block" } }}
+            image="https://i5.walmartimages.com/asr/9b971d54-7995-4a47-aa7a-adb2d7630c6c.f21033ccb62a1d89e93c2402428e6085.jpeg"
+            alt="text"
+          />
+        </Card>
+      </CardActionArea>
+    </Grid>
   );
 }
 
