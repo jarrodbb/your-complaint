a7009523ed31bfe3c9b53f5011ba678773de2697
Jarrod Blanning <jarrod.blanning@gmail.com>
2023-10-26 12:47:59 +1000

    add complaint. added date picker and image

--- client/src/components/complaintForm/complaintForm.jsx
+++ client/src/components/complaintForm/complaintForm.jsx
@@ -1,26 +1,17 @@
 import React, { useState } from "react";
-import { useDropzone } from "react-dropzone";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+// import base64 from 'react-native-base64'
 import { useMutation } from "@apollo/client";
-// import { gql } from "@apollo/client";
+
 import { CREATE_COMPLAINT } from "../../utils/mutations";
 
 // Import PropTypes validation
 import PropTypes from "prop-types";
 import { GET_COMPLAINTS } from "../../utils/queries";
 
-// Define GraphQL mutation to upload an image as an attachment
-// const UPLOAD_IMAGE = gql`
-//   mutation uploadImage($file: Upload!) {
-//     uploadImage(file: $file) {
-//       filename
-//       mimetype
-//       encoding
-//     }
-//   }
-// `;
-
 // Define React functional component called 'ComplaintForm' which takes 'closeModal' as a prop.
-const ComplaintForm = ({ closeModal}) => {
+const ComplaintForm = ({ closeModal }) => {
   const [addComplaint, { error }] = useMutation(CREATE_COMPLAINT);
 
   // 'category' state variable tracks the selected category for the complaint, with 'General' as the initial value.
@@ -28,7 +19,11 @@ const ComplaintForm = ({ closeModal}) => {
   // 'complaintText' state variable stores the text of the complaint.
   const [complaintText, setComplaintText] = useState("");
   // 'image' state variable is used to store an uploaded image (initially set to 'null').
-  // const [image, setImage] = useState(null);
+  const [image, setImage] = useState(null);
+
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const [selectedDate, setSelectedDate] = useState(null);
 
   // const [uploadImage] = useMutation(UPLOAD_IMAGE);
 
@@ -39,58 +34,54 @@ const ComplaintForm = ({ closeModal}) => {
 
   // 'handleTextChange' function updates the 'complaintText' state as the user types their complaint.
   const handleTextChange = (e) => {
-    setComplaintText(e.target.value);
+    const { target } = e;
+    const inputType = target.name;
+    const inputValue = target.value;
+    if (inputValue === "") {
+      return (
+        setErrorMessage("Please include a description"), setComplaintText("")
+      );
+    } else {
+      setComplaintText(e.target.value);
+    }
   };
-
-  // 'onDrop' function is called when the user drops an image into the dropzone. It handles the uploaded image files.
-  // const onDrop = (acceptedFiles) => {
-  //   if (acceptedFiles.length > 0) {
-  //     setImage(acceptedFiles[0]);
-  //   }
-  // };
 
   // 'handleSubmit' function is called when the user submits the form.
   const handleSubmit = async (e) => {
     e.preventDefault();
+
     try {
       const { data } = await addComplaint({
         variables: {
           description: complaintText,
           category: category,
+          image: image,
+          date: selectedDate,
         },
       });
       setCategory("General");
-
+      setImage("");
       complaintText("");
+      setSelectedDate(null);
     } catch (err) {
       console.error(err);
     }
 
-    // Check if image is uploaded
-    // if (image) {
-    //   try {
-    //     // Upload image and get response
-    //     const { data } = await uploadImage({ variables: { file: image } });
-
-    //     // Check if image upload was successful
-    //     if (data.uploadImage) {
-    //       // Extract the uploaded image details
-    //       const { filename, mimetype, encoding } = data.uploadImage;
-    //     }
-    //   } catch (error) {
-    //     console.error("Image upload error:", error);
-    //   }
-    // }
-    // console.log(image);
-    // Close the modal after submission
     closeModal();
   };
 
-  // 'useDropzone' hook provides dropzone functionality.
-  // const { getRootProps, getInputProps } = useDropzone({
-  //   onDrop,
-  //   accept: "image/*, video/*",
-  // });
+  function convertToBase64(e) {
+    console.log(e);
+    let reader = new FileReader();
+    reader.readAsDataURL(e.target.files[0]);
+    reader.onload = () => {
+      console.log(reader.result);
+      setImage(reader.result);
+    };
+    reader.onerror = (error) => {
+      console.log("error: ", error);
+    };
+  }
 
   // Render complaint form within a <div> element.
   return (
@@ -114,26 +105,43 @@ const ComplaintForm = ({ closeModal}) => {
         <textarea
           placeholder="Type your complaint here"
           value={complaintText}
+          name="description"
+          type="text"
+          required
           onChange={handleTextChange}
         />
-
-        {/* Dropzone for image upload */}
-        {/* <div {...getRootProps()} className="dropzone">
-          <input {...getInputProps()} />
-          <p>Drag 'n' drop an image here, or click to select one</p>
-        </div> */}
-
-        {/* Display the uploaded image if available */}
-        {/* {image && (
-          <div className="uploaded-image">
-            <p>Uploaded Image:</p>
-            <img src={URL.createObjectURL(image)} alt="Uploaded" />
+        <div className="App">
+          <DatePicker
+            selected={selectedDate}
+            placeholderText="When did it happen"
+            onChange={(date) => setSelectedDate(date)}
+            dateFormat="dd/MM/yyyy"
+            maxDate={new Date()}
+            isClearable
+            showYearDropdown
+            scrollableMonthYearDropdown
+          />
+        </div>
+        <div className="auth-wrapper">
+          <div className="auth-inner" style={{ width: "auto" }}>
+            Upload an image <br />
+            <input accept="image/*" type="file" onChange={convertToBase64} />
+            {image === "" || image == null ? (
+              ""
+            ) : (
+              <img width={100} height={100} src={image} />
+            )}
           </div>
-        )} */}
+        </div>
 
         {/* Submission button */}
         <button type="submit">Submit Complaint</button>
       </form>
+      {errorMessage && (
+        <div>
+          <p className="error-text p-2 error-display mt-2">{errorMessage}</p>
+        </div>
+      )}
     </div>
   );
 };
