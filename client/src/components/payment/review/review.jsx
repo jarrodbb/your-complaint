7dfa0e388ed6e11e2546cde159246eda30e9412d
MakeRedundant <105352286+MakeRedundant@users.noreply.github.com>
2023-10-26 20:54:49 +1100

    added contact + donate stuff

---
+++ client/src/components/payment/review/review.jsx
@@ -0,0 +1,89 @@
+import * as React from "react";
+import Typography from "@mui/material/Typography";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import Grid from "@mui/material/Grid";
+
+const products = [
+  {
+    name: "YOUR SOUL",
+    desc: "Worthless",
+    price: "$0.99",
+  },
+  {
+    name: "YOUR LIFE SAVINGS",
+    desc: "Very nice",
+    price: "$1.45",
+  },
+  {
+    name: "Everything else",
+    desc: "NFTs included",
+    price: "$9.95",
+  },
+  {
+    name: "CEO's new mega yacht",
+    desc: "NO TAX",
+    price: "$9999999",
+  },
+
+  { name: "Shipping", desc: "", price: "Free" },
+];
+
+const addresses = [" 9 Merge Conflict", "Javascript", "Cental", "99999", "Sydney"];
+const payments = [
+  { name: "Card type", detail: "Visa" },
+  { name: "Card holder", detail: "Mr Complain Alot" },
+  { name: "Card number", detail: "xxxx-xxxx-xxxx-1234" },
+  { name: "Expiry date", detail: "04/2099" },
+];
+
+export default function Review() {
+  return (
+    <React.Fragment>
+      <Typography variant="h6" gutterBottom>
+        Order summary
+      </Typography>
+      <List disablePadding>
+        {products.map((product) => (
+          <ListItem key={product.name} sx={{ py: 1, px: 0 }}>
+            <ListItemText primary={product.name} secondary={product.desc} />
+            <Typography variant="body2">{product.price}</Typography>
+          </ListItem>
+        ))}
+        <ListItem sx={{ py: 1, px: 0 }}>
+          <ListItemText primary="Total" />
+          <Typography variant="subtitle1" sx={{ fontWeight: 700 }}>
+            $99999999
+          </Typography>
+        </ListItem>
+      </List>
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={6}>
+          <Typography variant="h6" gutterBottom sx={{ mt: 2 }}>
+            Shipping
+          </Typography>
+          <Typography gutterBottom>John Smith</Typography>
+          <Typography gutterBottom>{addresses.join(", ")}</Typography>
+        </Grid>
+        <Grid item container direction="column" xs={12} sm={6}>
+          <Typography variant="h6" gutterBottom sx={{ mt: 2 }}>
+            Payment details
+          </Typography>
+          <Grid container>
+            {payments.map((payment) => (
+              <React.Fragment key={payment.name}>
+                <Grid item xs={6}>
+                  <Typography gutterBottom>{payment.name}</Typography>
+                </Grid>
+                <Grid item xs={6}>
+                  <Typography gutterBottom>{payment.detail}</Typography>
+                </Grid>
+              </React.Fragment>
+            ))}
+          </Grid>
+        </Grid>
+      </Grid>
+    </React.Fragment>
+  );
+}
--- client/src/pages/contact/contact.jsx
+++ client/src/pages/contact/contact.jsx
@@ -1,25 +1,237 @@
 import "./contact.css";
+import React, { useState } from "react";
+import { Form, Input, TextArea, Button } from "semantic-ui-react";
+//Form, Input, TextArea, and Button are components from the Semantic UI React library for building forms.
+import emailjs from "emailjs-com";
+//emailjs is a library for sending emails using JavaScript.
+import Swal from "sweetalert2";
+//Swal is an external library for displaying pop-up alerts (SweetAlert2).
+import { validateEmail } from "../../components/utils/Email Validate/helpers";
+// import "semantic-ui-css/semantic.min.css";
 
-const Contact = () => {
+
+//EmailJS
+///Constant that stores the required IDs for sending emails via the EmailJS Service
+const TEMPLATE_ID = "template_4aipo27";
+const USER_ID = "dNo7JEuizWBnqYSMg";
+const SERVICE_ID = "service_lv2pvc8";
+
+// !!!!!Refer to this !!!!!!!!!!!!
+//https://plainenglish.io/blog/how-to-build-a-contact-form-in-react-that-sends-emails-using-emailjs-70011d2563a3
+
+export default function Contact() {
+  //Define states
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [message, setMessage] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [submission, setSubmission] = useState("");
+  const [userLocation, setLocation] = useState("");
+
+  const checkClick = (e) => {
+    const { target } = e;
+    const inputType = target.name;
+    const inputValue = target.value;
+    console.log(inputType);
+    if (userLocation === "") {
+    }
+  };
+
+  //Function to handle when input is changed
+  //Checks if input is empty and calls error message if required
+  const handleInputChange = (e) => {
+    const { target } = e;
+    const inputType = target.name;
+    const inputValue = target.value;
+
+    if (inputType === "email") {
+      if (inputValue === "") {
+        return setErrorMessage("cannot be empty"), setEmail("");
+      } else {
+        setEmail(inputValue);
+      }
+    }
+    if (inputType === "name") {
+      if (inputValue === "") {
+        return setErrorMessage("Please include your name"), setName("");
+      } else {
+        setName(inputValue);
+      }
+    }
+    if (inputType === "message") {
+      if (inputValue === "") {
+        return (
+          setErrorMessage("Please include a message, thanks"), setMessage("")
+        );
+      } else {
+        setMessage(inputValue);
+      }
+    }
+    setErrorMessage("");
+    setSubmission("");
+  };
+
+  //Handle when form is submitted
+  const handleFormSubmit = (e) => {
+    //prevent default on form
+    e.preventDefault();
+//Checks if email is valid and returns a error message if not
+    if (!validateEmail(email)) {
+      setErrorMessage("Error! Email is invalid, please try again");
+      return;
+    }
+    emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, e.target, USER_ID).then(
+      (result) => {
+        console.log(result.text);
+        Swal.fire({
+          icon: "success",
+          title: "Message Sent Successfully",
+        });
+      },
+      (error) => {
+        console.log(error.text);
+        Swal.fire({
+          icon: "error",
+          title: "Ooops, something went wrong",
+          text: error.text,
+        });
+      }
+    );
+    //Alerts user after successful message sent
+    setSubmission(`Thanks for your email ${name}. I'll be in contact 👍`);
+
+    setName("");
+    setMessage("");
+    setErrorMessage("");
+    setEmail("");
+  };
+  const checkName = (event) => {
+    console.log(event.target.value);
+    const { target } = event;
+    const inputType = target.name;
+    const inputValue = target.value;
+
+    if (inputValue.length < 1) {
+      setErrorMessage(`${inputType} cannot be empty`);
+    }
+  };
+  //Form here to handle name and message using emailjs
   return (
-    <section id='contact'>
+    <section id="contact">
       <h1>Contact</h1>
       <div className="contact-container">
         <div className="text-box">
           <div className="contact-text">
-            <p> Need to complain to the team? <br /> Feel free to contact us here</p>
+            <p>
+              Need to complain to the team? <br /> Feel free to contact us here
+            </p>
           </div>
         </div>
         <div className="contact-details">
           {/* All links open in a new tab */}
-          <a href="tel:+9999999999" className="contact-button"><i className="fas fa-phone"></i> Phone</a>
-          <a href="https://makeredundant.github.io/Brian-Website/" className="contact-button" target="_blank" rel="noreferrer"><i className="fas fa-globe"></i> Website</a>
-          <a href="mailto:Brian.trang@hotmail.com" className="contact-button"><i className="fas fa-envelope"></i> Email</a>
-          <a href="https://github.com/MakeRedundant" className="contact-button" target="_blank" rel="noreferrer"><i className="fab fa-github"></i> GitHub</a>
+          <a href="tel:+9999999999" className="contact-button">
+            <i className="fas fa-phone"></i> Phone
+          </a>
+          <a
+            href="https://makeredundant.github.io/Brian-Website/"
+            className="contact-button"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i className="fas fa-globe"></i> Website
+          </a>
+          <a href="mailto:Brian.trang@hotmail.com" className="contact-button">
+            <i className="fas fa-envelope"></i> Email
+          </a>
+          <a
+            href="https://github.com/MakeRedundant"
+            className="contact-button"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i className="fab fa-github"></i> GitHub
+          </a>
         </div>
+      </div>
+      <div className="title-boarder rounded-circle">
+        <h1>Email me here!</h1>
+      </div>
+      <div className="App">
+        <h3>{submission}</h3>
+        <Form className="contact-bg" onSubmit={handleFormSubmit}>
+          <Form.Field
+            id="form-input-control-last-name"
+            value={name}
+            name="name"
+            control={Input}
+            onBlur={checkName}
+            onChange={handleInputChange}
+            label="Name"
+            type="text"
+            onClick={checkClick}
+            placeholder="Name..."
+            required
+            icon="user circle"
+            iconPosition="left"
+          />
+          <Form.Field
+            id="form-input-control-email"
+            value={email}
+            control={Input}
+            name="email"
+            onBlur={checkName}
+            label="Email"
+            onChange={handleInputChange}
+            type="email"
+            placeholder="Email"
+            required
+            icon="mail"
+            iconPosition="left"
+          />
+          <TextArea
+            id="form-textarea-control-opinion"
+            rows="5"
+            value={message}
+            onBlur={checkName}
+            name="message"
+            label="Message"
+            onChange={handleInputChange}
+            type="text"
+            placeholder="Message here"
+            required
+          />
+          <button className="submit-button mt-2" type="submit">
+            Submit
+          </button>
+        </Form>
+        {errorMessage && (
+          <div>
+            <p className="error-text p-2 error-display mt-2">{errorMessage}</p>
+          </div>
+        )}
       </div>
     </section>
   );
-};
+}
 
-export default Contact;
+// const Contact = () => {
+//   return (
+//     <section id='contact'>
+//       <h1>Contact</h1>
+//       <div className="contact-container">
+//         <div className="text-box">
+//           <div className="contact-text">
+//             <p> Need to complain to the team? <br /> Feel free to contact us here</p>
+//           </div>
+//         </div>
+//         <div className="contact-details">
+//           {/* All links open in a new tab */}
+//           <a href="tel:+9999999999" className="contact-button"><i className="fas fa-phone"></i> Phone</a>
+//           <a href="https://makeredundant.github.io/Brian-Website/" className="contact-button" target="_blank" rel="noreferrer"><i className="fas fa-globe"></i> Website</a>
+//           <a href="mailto:Brian.trang@hotmail.com" className="contact-button"><i className="fas fa-envelope"></i> Email</a>
+//           <a href="https://github.com/MakeRedundant" className="contact-button" target="_blank" rel="noreferrer"><i className="fab fa-github"></i> GitHub</a>
+//         </div>
+//       </div>
+//     </section>
+//   );
+// };
