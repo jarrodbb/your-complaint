a18fc9e8cae8a997a62936415b52b6d61e383db0
MakeRedundant <105352286+MakeRedundant@users.noreply.github.com>
2023-10-28 16:35:29 +1100

    fixed Profile.jsx added some MUI magic

--- client/src/pages/profile/profile.jsx
+++ client/src/pages/profile/profile.jsx
@@ -6,6 +6,14 @@ import { useQuery } from "@apollo/client";
 import { GET_ME } from "../../utils/queries";
 import Auth from "../../utils/auth";
 
+import Grid from "@mui/material/Grid";
+import PropTypes from "prop-types";
+import Typography from "@mui/material/Typography";
+import Card from "@mui/material/Card";
+import CardActionArea from "@mui/material/CardActionArea";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+
 const Profile = () => {
   const { loading, data } = useQuery(GET_ME);
 
@@ -15,33 +23,58 @@ const Profile = () => {
 
   const userData = data?.me || {};
   const complaints = userData.complaints || [];
-  console.log("User Data is", userData);
+  console.log("User Data is", userData); //Should return the object with id username and stuff
+
+  const defaultImageLink = "https://source.unsplash.com/random?wallpapers";
 
   return (
     <section id="profile">
-      <h1>Profile</h1>
+      <Typography variant="h1">Profile</Typography>
       {Auth.loggedIn() ? (
         <div>
-          <p> Your: User ID: {userData._id}</p>
-          <h2>Complaints:</h2>
-          <ul>
-            {complaints.map((complaint) => (
-              <li key={complaint._id}>
-                <strong>Title:</strong> {complaint.title}
-                <br />
-                Description: {complaint.description}
-                <br />
-                Category: {complaint.category}
-                <hr />
-              </li>
-            ))}
-          </ul>
+          <Typography variant="body1">
+            Your User ID: <strong>{userData._id}</strong>
+          </Typography>
+          <Typography variant="h2"> Your Complaints:</Typography>
+          {complaints.map((complaint) => (
+            <CardActionArea
+              component={Link}
+              to={`/Complaint/${complaint._id}`}
+              key={complaint._id}
+            >
+              <Card sx={{ display: "flex", width: "100%" }}>
+                <CardContent sx={{ flex: 1 }}>
+                  <Typography component="h2" variant="h5">
+                    {complaint.title}
+                  </Typography>
+                  <Typography variant="subtitle1" color="text.secondary">
+                    {complaint.date}
+                  </Typography>
+                  <Typography variant="subtitle1" paragraph>
+                    {complaint.description}
+                  </Typography>
+                  <Typography variant="subtitle1" color="primary">
+                    Continue reading...
+                  </Typography>
+                </CardContent>
+                <CardMedia
+                  component="img"
+                  sx={{
+                    width: 160,
+                    display: { xs: "none", sm: "block" },
+                  }}
+                  image={complaint.image || defaultImageLink}
+                  alt="text"
+                />
+              </Card>
+            </CardActionArea>
+          ))}
         </div>
-      ) : (
-        <p>
+      ) : ( //Conditional statement if user is not logged in will render this
+        <Typography variant="body1">
           You need to be logged in to view your profile. Please{" "}
           <Link to="/SignIn">login</Link> or <Link to="/SignUp">signup.</Link>
-        </p>
+        </Typography>
       )}
     </section>
   );
